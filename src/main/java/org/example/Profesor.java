7b6728ff76e6d39ec69b44cc37f727e22ad2f01b
Andreea Simona <92686562+Luz1210@users.noreply.github.com>
2024-01-22 15:08:10 +0200

    added classes profesor and student

---
+++ src/main/java/org/example/Profesor.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Profesor {
+    //functions that will be used by the professor
+    //de fiecare data cand un profesor face modificari in tabela nota, media din tabela disciplina trebuie recalculata
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Student {
+    //functions that will be used by the student
+}
